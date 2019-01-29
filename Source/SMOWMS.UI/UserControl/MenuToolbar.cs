75775c08c62b3645b99edbfa6834154c63f162d1
lula <lula.jin@smobiler.com>
2019-01-29 14:14:28 +0800

    修复toolbar跳转报错

--- Source/SMOWMS.UI/UserControl/MenuToolbar.cs
+++ Source/SMOWMS.UI/UserControl/MenuToolbar.cs
@@ -34,6 +34,11 @@ namespace SMOWMS.UI.UserControl
                 toolBar.SelectedIndex = value;
             }
         }
+        [Browsable(false), Category("Appearance"), DefaultValue(""), Description("当前选中项的name")]
+        public string selectName
+        {
+            set; get;
+        }
         /// <summary>
         /// 菜单点击事件
         /// </summary>
@@ -41,32 +46,76 @@ namespace SMOWMS.UI.UserControl
         /// <param name="e"></param>
         private void toolBar_ToolbarItemClick(object sender, ToolbarClickEventArgs e)
         {
-            switch (e.Name)
+            selectName = e.Name;
+            switch (this.Form.Name)
+            {
+                case "frmMenu":
+                    showForm(e.Name);
+                    break;
+                case "frmOrder":
+                    if (e.Name != "订单") this.Form.Close();
+                    break;
+                case "frmMasterData":
+                    if (e.Name != "主数据") this.Form.Close();
+                    break;
+                case "frmAnalyze":
+                    if (e.Name != "统计") this.Form.Close();
+                    break;
+                case "frmMessage":
+                    if (e.Name != "设置") this.Form.Close();
+                    break;
+            }
+
+
+        }
+        private void showForm(string ename)
+        {
+            switch (ename)
             {
                 case "仓库":
-                    frmMenu frmMenu = new frmMenu();
-                    Form.Close();
-                    this.Form.Show(frmMenu);
+                    // frmMenu frmMenu = new frmMenu();
+                    //this.Form.Show(frmMenu, (obj, arg) =>
+                    //{
+                    //    showForm(frmMenu.menuToolbar.selectName);
+                    //});
+                    //Form.Close();
+                    //this.Form.Show(frmMenu);
                     break;
                 case "订单":
                     frmOrder frmOrder = new frmOrder();
-                    Form.Close();
-                    this.Form.Show(frmOrder);
+                    this.Form.Show(frmOrder, (obj, arg) =>
+                    {
+                        showForm(frmOrder.menuToolbar.selectName);
+                    });
+                    //Form.Close();
+                    //this.Form.Show(frmOrder);
                     break;
                 case "主数据":
                     frmMasterData frmMasterData = new frmMasterData();
-                    Form.Close();
-                    this.Form.Show(frmMasterData);
+                    this.Form.Show(frmMasterData, (obj, arg) =>
+                    {
+                        showForm(frmMasterData.menuToolbar.selectName);
+                    });
+                    //Form.Close();
+                    //this.Form.Show(frmMasterData);
                     break;
                 case "统计":
                     frmAnalyze frmAnalyse = new frmAnalyze();
-                    Form.Close();
-                    this.Form.Show(frmAnalyse);
+                    this.Form.Show(frmAnalyse, (obj, arg) =>
+                    {
+                        showForm(frmAnalyse.menuToolbar.selectName);
+                    });
+                    //Form.Close();
+                    //this.Form.Show(frmAnalyse);
                     break;
                 case "设置":
                     frmMessage frmMessage = new frmMessage();
-                    Form.Close();
-                    this.Form.Show(frmMessage);
+                    this.Form.Show(frmMessage, (obj, arg) =>
+                    {
+                        showForm(frmMessage.menuToolbar.selectName);
+                    });
+                    //Form.Close();
+                    //this.Form.Show(frmMessage);
                     break;
             }
         }
